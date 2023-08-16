8fe0564d692765383b6fd4fcbe457aa39e5cf7d6
AustinRado <radoaussy@gmail.com>
2023-08-16 13:47:08 -0700

    only diff elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
 def only_diff_elements(set_1, set_2):
-    uni_set = set_1.symmetric_difference(set_2)
-
-    return(uni_set)
+    return set_1.union(set_2).difference(set_1.intersection(set_2)))
