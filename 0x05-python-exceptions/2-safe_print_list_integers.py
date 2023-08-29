67449397e7b7f0ca769734c2d2a1c75a24dc9ad4
AustinRado <radoaussy@gmail.com>
2023-08-29 05:22:00 +0300

    initial commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    printed_integers = 0
+    for i in range(x):
+        try:
+            value = my_list[i]
+            print("{:d}".format(value), end='')
+            printed_integers += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+
+    print()
+    return printed_integers
