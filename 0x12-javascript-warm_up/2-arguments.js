34fa90a2bbd20fc73c935280ea21fb696d8a71b9
AustinRado <radoaussy@gmail.com>
2023-11-14 01:53:29 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const numArgs = process.argv.length - 2;
+const numArguments = process.argv.length - 2;
 
-if (numArgs === 0){
-    console.log('No argument');
-} else if (numArgs === 1){
-    console.log('Argument found');
-}else {
-    console.log('Arguments found');
+if (numArguments === 0) {
+  console.log('No argument');
+} else if (numArguments === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
