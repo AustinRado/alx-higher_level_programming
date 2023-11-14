6c70a78e91a8290f570889436f996dee6ce0821d
AustinRado <radoaussy@gmail.com>
2023-11-15 00:11:30 +0300

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,16 +1,7 @@
 #!/usr/bin/nodes
 
 const fs = require('fs');
-const file1 = process.argv[2];
-const file2 = process.argv[3];
 
-const content1 = fs.readFileSync(file1);
-const content2 = fs.readFileSync(file2);
-
-const contentNew = content1 + content2;
-
-fs.writeFile(process.argv[4], contentNew, 'utf-8', (err) => {
-  if (err) {
-    throw err;
-  }
-});
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
