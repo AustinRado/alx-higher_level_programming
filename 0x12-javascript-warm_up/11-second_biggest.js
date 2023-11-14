51012a7f5987974aadf19ba6207847a0ca19bce4
AustinRado <radoaussy@gmail.com>
2023-11-14 04:12:45 +0300

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const process = require('process');
 const args = process.argv;
 if (args.length < 4) {
-    console.log(0);
+  console.log(0);
 } else {
-    console.log(args.sort().reverse()[1]);
+  console.log(args.sort().reverse()[1]);
 }
