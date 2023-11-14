560cf8ad178b578552663db558ded59aae9ebb92
AustinRado <radoaussy@gmail.com>
2023-11-14 03:29:57 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const str = ['C is fun','Python is cool','JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < str.length; i++){
-    console.log(str[i]);
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
