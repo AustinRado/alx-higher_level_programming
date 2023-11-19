6edc642540baa856c262cc62cc142202643e69a6
AustinRado <radoaussy@gmail.com>
2023-11-19 20:19:09 +0300

    changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,7 @@
 import MySQLdb
 from sys import argv
 
+
 if __name__ == '__main__':
     database = MySQLdb.connect(port=3306,
                                host='localhost',
@@ -12,13 +13,12 @@ if __name__ == '__main__':
                                passwd=argv[2],
                                db=argv[3])
     try:
-       curs = database.cursor()
-       curs.execute("SELECT * FROM states ORDER BY id ASC")
-       rows = curs.fetchall()
-       for row in rows:
-         print(row)
-       curs.close()
-
+        curs = database.cursor()
+        curs.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = curs.fetchall()
+        for row in rows:
+            print(row)
+        curs.close()
     except MySQLdb.Error as e:
-       print(f"Error fetching data from the database: {e}")
+        print(f"Error fetching data from the database: {e}")
     database.close()
