29901f4992e6ce197d18c04eb3085d239f6c2e97
AustinRado <radoaussy@gmail.com>
2024-01-03 10:35:00 -0800

    executable

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,35 +1,22 @@
 #!/usr/bin/node
-
 const request = require('request');
-const id = process.argv[2];
-const endpoint = `https://swapi-api.alx-tools.com/api/films/${id}`;
-const opt = { method: 'GET' };
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-/**
- * Get name of a character from starwars using promises
- * @param {*} url url string
- * @returns Promise<string> (name)
- */
-const getName = (url) => new Promise((resolve, reject) => {
-  request(url, opt, (err, { statusCode: code, body: res }) => {
-    if (err) reject(console.log(err));
-    if (code === 200) {
-      resolve(JSON.parse(res).name);
-    } else {
-      resolve(console.log(`Error code: ${code}`));
-    }
-  });
-});
+request(url, async (error, response, body) => {
+  if (error) { console.log(error); }
+  const result = JSON.parse(body);
 
-request(endpoint, opt, (err, { statusCode, body }) => {
-  if (err) return console.log(err);
-  if (statusCode === 200) {
-    const { characters } = JSON.parse(body);
-    const names = characters.map(url => getName(url));
-    Promise.all(names)
-      .then(names => names.forEach(name => console.log(name)));
-  } else {
-    console.log(`Error code: ${statusCode}`);
+  for (const charURL of result.characters) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, r, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
   }
 });
-
