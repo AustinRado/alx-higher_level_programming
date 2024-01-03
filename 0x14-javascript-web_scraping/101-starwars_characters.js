af2adb204a4ff48179d438f7e96e78b79a4a3d10
AustinRado <radoaussy@gmail.com>
2024-01-03 10:25:58 -0800

    chnages

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
+request(url, async (error, response, body) => {
+  if (error) { console.log(error); }
   const result = JSON.parse(body);
+
   for (const charURL of result.characters) {
-    request(charURL, (err, r, body) => {
-      if (err) {
-        console.log(err);
-      }
-      const result = JSON.parse(body);
-      console.log(result.name);
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, r, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
     });
   }
 });
